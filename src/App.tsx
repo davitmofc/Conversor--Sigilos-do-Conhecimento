76c06c20fd987e8d64ef620427e804a57011f0a1
Davi <038davi@gmail.com>
2025-02-05 13:50:28 -0300

    Atualizar o App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -259,7 +259,7 @@ useEffect(() => {
               Esse programa está em desenvolvimento e esta é a primeira versão.
               O seu feedback é muito importante! Envie-o através da{" "}
               <a
-                href="https://www.reddit.com/r/OrdemParanormalRPG/s/BNN5x5G0KP"
+                href="https://www.reddit.com/r/OrdemParanormalRPG/s/P31OIkOg4z"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{
